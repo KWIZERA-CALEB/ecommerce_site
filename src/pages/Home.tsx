cca47b8deadb1ec8e0b1401216507218f048bae7
Kwizera Caleb <your email\a>
2024-07-19 12:36:55 +0200

    [New Frontend Updates]: I reorganized the code

---
+++ src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react'
+import { fetchProducts } from "../services/productservice"
+import { Product } from '../utils/producttypes'
+
+const Home: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(()=> {
+    const getProducts = async()=> {
+      try {
+        const productsData = await fetchProducts()
+        setProducts(productsData)
+      }catch(error) {
+        console.log('Failed to fetch products')
+      }
+    }
+
+    //call the getProducts fnc
+    getProducts()
+
+  }, [])
+
+  const renderedProducts = []
+  for (let i = 0; i < products.length; i++) {
+      renderedProducts.push(
+        <h3 key={products[i].id}>Product Name: {products[i].product_name}</h3>
+      )
+  }
+
+
+  return (
+    <div>
+      Home
+      <div>
+        {products.map((product)=> (
+          <h3 key={product.id}>{product.product_name}</h3>
+        ))}
+      </div>
+      For loop
+      <div>
+        {renderedProducts}
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/utils/producttypes.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    id: number,
+    product_name: string,
+    product_price: any,
+    description: string
+}
